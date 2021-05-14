9aa9e4d1edb77d16a3d7053608a794669813fbc3
Boston Moywaywa <OMAX1139@OMX1139>
2021-05-14 08:45:15 -0700

    Add project files.

---
+++ CAPP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+
+namespace CAPP
+{
+    using (MySqlConnection con = new MySqlConnection())
+            {
+                con.ConnectionString = "Server=localhost;Database=shopping;Uid=root;Pwd=Bosaul23@;"; 
+                MySqlCommand cmd = new MySqlCommand();
+                 cmd.CommandText = "Select *  from shopmanager";
+   
+                cmd.Connection = con; 
+                con.Open(); 
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0]);
+                    Console.WriteLine(reader[1]);
+                    Console.WriteLine(reader[2]);
+                    Console.WriteLine(reader[3]);
+                    Console.WriteLine(reader[4]);
+                }
+Console.ReadLine();
+}
+
+ 
